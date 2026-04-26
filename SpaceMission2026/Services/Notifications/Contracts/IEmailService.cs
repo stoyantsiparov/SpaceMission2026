e430ed40aa1cfe6648c1071a3d29d833927ea66d
Stoyan Tsiparov <133793181+stoyantsiparov@users.noreply.github.com>
2026-04-26 11:45:44 +0300

    Implement SMTP email service for mission reporting

---
+++ SpaceMission2026/Services/Notifications/Contracts/IEmailService.cs
@@ -0,0 +1,20 @@
+﻿using SpaceMission2026.Core.Models;
+
+namespace SpaceMission2026.Services.Notifications.Contracts;
+
+/// <summary>
+/// Defines the contract for dispatching mission reports to Mission Control.
+/// </summary>
+public interface IEmailService
+{
+    /// <summary>
+    /// Sends an email report detailing the outcome of each astronaut's journey.
+    /// </summary>
+    /// <param name="smtpHost">The SMTP server host address.</param>
+    /// <param name="smtpPort">The SMTP server port.</param>
+    /// <param name="senderEmail">The email address used to authenticate and send the email.</param>
+    /// <param name="password">The password or app-specific password for the sender's email.</param>
+    /// <param name="receiverEmail">The destination email address.</param>
+    /// <param name="astronauts">The list of astronauts with their mission status.</param>
+    void SendReport(string smtpHost, int smtpPort, string senderEmail, string password, string receiverEmail, IEnumerable<Astronaut> astronauts);
+}
---
+++ SpaceMission2026/Services/Notifications/SmtpEmailService.cs
@@ -0,0 +1,54 @@
+﻿using System.Net;
+using System.Net.Mail;
+using System.Text;
+using SpaceMission2026.Common;
+using SpaceMission2026.Core.Models;
+using SpaceMission2026.Services.Notifications.Contracts;
+
+namespace SpaceMission2026.Services.Notifications;
+
+public class SmtpEmailService : IEmailService
+{
+    /// <inheritdoc />
+    public void SendReport(string smtpHost, int smtpPort, string senderEmail, string password, string receiverEmail, IEnumerable<Astronaut> astronauts)
+    {
+        try
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine(AppMessages.EmailReportTitle);
+            sb.AppendLine(new string('-', 30));
+
+            foreach (var astronaut in astronauts.OrderByDescending(a => a.IsRescued).ThenBy(a => a.TotalPathCost))
+            {
+                string note = astronaut.IsRescued
+                    ? string.Format(AppMessages.EmailRescuedNote, astronaut.Id, astronaut.TotalPathCost)
+                    : string.Format(AppMessages.EmailLostNote, astronaut.Id);
+
+                sb.AppendLine(note);
+            }
+
+            using var client = new SmtpClient(smtpHost, smtpPort)
+            {
+                Credentials = new NetworkCredential(senderEmail, password),
+                EnableSsl = true
+            };
+
+            var mailMessage = new MailMessage
+            {
+                From = new MailAddress(senderEmail),
+                Subject = AppMessages.EmailSubject,
+                Body = sb.ToString(),
+                IsBodyHtml = false
+            };
+
+            mailMessage.To.Add(receiverEmail);
+            client.Send(mailMessage);
+
+            Console.WriteLine(AppMessages.EmailSentSuccess);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(string.Format(AppMessages.EmailWarning, ex.Message));
+        }
+    }
+}
